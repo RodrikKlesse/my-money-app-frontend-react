41d6a5aae56a0ca2ff7364c479f58972aa484bc2
RodrikKlesse <rodrik_hsk@hotmail.com>
2020-01-17 11:46:23 -0300

    Criado o form incial e feito as tratativas dos erros

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -8,6 +8,18 @@ class BillingCycleList extends Component {
     componentWillMount() {
         this.props.getList()
     }
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
+    }
+
     render() {
         return (
             <div>
@@ -20,7 +32,7 @@ class BillingCycleList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
